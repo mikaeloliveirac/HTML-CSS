b89e91f390672bb6338dcd3a8d2413f42e71b3e9
Mikael Oliveira <95417621+mikaeloliveirac@users.noreply.github.com>
2021-12-08 14:28:29 -0300

    atualizei calculadora

--- exercicios/listas meus testes/calculadora/calc.js
+++ exercicios/listas meus testes/calculadora/calc.js
@@ -1,21 +1,18 @@
-function Calcular(){
-    let num1 = document.querySelector("input#txtn1")
-    let num2 = document.querySelector("input#txtn2")
-    let res = document.querySelector("div#res")
-    if(num1.value==0 || num2.value==0){
-        window.alert("[ERRO]")
-    }
-    else{
-        let n1=Number(num1.value)
-        let n2=Number(num2.value)
-
-        let soma = n1 +n2
-        let Dv = n1/n2
-        let sub = n1-n2
-        let mult = n1*n2
-        res.innerHTML= `A soma entre os números é ${soma}`
-    }
+function calc(num){
+   var res = document.getElementById("res").innerHTML
+   document.getElementById("res").innerHTML = res + num
+}
+function limpa(){
     
+    document.getElementById("res").innerHTML= ""
+}
+function volta(){
+    var res = document.getElementById("res").innerHTML
+    document.getElementById("res").innerHTML = res.substring(0,res.length -1);
+}
+function igual(){
+    var resultado = document.getElementById("res").innerHTML
+    document.getElementById("res").innerHTML = eval(resultado)
 }
 
 
